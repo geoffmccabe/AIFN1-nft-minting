d2396b6df4ff4d631a2a6cb22ba7dd2d79e08e63
geoffmccabe <33639939+geoffmccabe@users.noreply.github.com>
2025-03-23 09:17:31 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// Configuration for AIFN1-nft-minting frontend
+const blockchainConfig = {
+    sepolia: {
+        contractAddress: "0x526383e54d97e65e7cc447c467dac5d37fa32dc9", // New deployed contract
+        mintFee: "0.001", // 0.001 ETH on Sepolia
+        gasLimit: 300000 // Base gas limit, overridden by estimation
+    }
+};
