db0ad0e1eaf399ddfa2abb0f63dcb13cb9f331f6
geoffmccabe <33639939+geoffmccabe@users.noreply.github.com>
2025-03-23 13:05:24 -0600

    Create entry.js

---
+++ entry.js
@@ -0,0 +1,14 @@
+// entry.js
+
+// Import pako from node_modules (pako is now installed)
+import "pako";
+
+// Import all the modules needed for PSD parsing
+import "./psd.js";
+import "./additionalInfo.js";
+import "./imageResources.js";
+import "./psdReader.js";
+import "./psdWriter.js";
+import "./helpers.js";
+import "./base64-js.js";
+import "./ag-psd.js";
